8bd16664b256739ea6b7a3041f1cfbe2d6bd96cb
strdos <81235866+strdos@users.noreply.github.com>
2023-12-03 22:16:49 +0100

    add lombock annotations, allure dependencies

--- src/test/java/com/spotify/oauth2/pojo/Error.java
+++ src/test/java/com/spotify/oauth2/pojo/Error.java
@@ -2,23 +2,20 @@ package com.spotify.oauth2.pojo;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.jackson.Jacksonized;
 
+@Getter @Setter
+@Jacksonized
+@Builder
 @JsonInclude(JsonInclude.Include.NON_NULL)
 
 public class Error {
 
     @JsonProperty("error")
-    private InnerError innerError;
-
-    @JsonProperty("error")
-    public InnerError getError() {
-        return innerError;
-    }
-
-    @JsonProperty("error")
-    public void setError(InnerError innerError) {
-        this.innerError = innerError;
-    }
+    private InnerError error;
 
 }
 
--- src/test/java/com/spotify/oauth2/tests/PlaylistTests.java
+++ src/test/java/com/spotify/oauth2/tests/PlaylistTests.java
@@ -26,17 +26,25 @@ public class PlaylistTests {
 
     public Playlist playlistBuilder(String name, String description, boolean _public) {
 
-        return new Playlist().
-                setName(name).
-                setDescription(description).
-                setPublic(_public);
+/*        Playlist playlist = new Playlist();
+        playlist.setName(name);
+        playlist.setDescription(description);
+        playlist.set_public(_public);
+        return playlist;*/
+
+        return Playlist.builder().
+                name(name).
+                description(description).
+                _public(_public).
+                build();
     }
 
     public void assertPlaylistEqual(Playlist responsePlaylist, Playlist requestPlaylist) {
 
         assertThat(responsePlaylist.getName(), equalTo(requestPlaylist.getName()));
         assertThat(responsePlaylist.getDescription(), equalTo(requestPlaylist.getDescription()));
-        assertThat(responsePlaylist.getPublic(), equalTo(requestPlaylist.getPublic()));
+        //assertThat(responsePlaylist.getPublic(), equalTo(requestPlaylist.getPublic()));
+        assertThat(responsePlaylist.get_public(), equalTo(requestPlaylist.get_public()));
     }
 
     public void assertStatusCode(int actualStatusCode, int expectedStatusCode) {
